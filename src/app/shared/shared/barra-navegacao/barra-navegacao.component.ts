dab864879c676e3fa64ad08803b0f405e24a6883
matheusfinoti <matheusfinoti@gmail.com>
2023-03-07 11:33:19 -0300

    Commit Japão - tabela quase pronta

---
+++ src/app/shared/shared/barra-navegacao/barra-navegacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-navegacao',
+  templateUrl: './barra-navegacao.component.html',
+  styleUrls: ['./barra-navegacao.component.scss']
+})
+export class BarraNavegacaoComponent {
+
+}
